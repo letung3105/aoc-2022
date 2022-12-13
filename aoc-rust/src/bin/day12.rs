d28d76eb69bf779e5371c9b69a71e5f0f62ecd41
letung3105 <15053897+letung3105@users.noreply.github.com>
2022-12-13 22:15:04 +0700

    optimize day 12

--- aoc-rust/src/bin/day12.rs
+++ aoc-rust/src/bin/day12.rs
@@ -29,33 +29,24 @@ impl Ord for DijkstraState {
     }
 }
 
-fn dijkstra(
-    adjacency_list: &[Vec<Vec<Position>>],
-    position_start: Position,
-    position_end: Position,
-) -> usize {
+fn dijkstra(adjacency_list: &[Vec<Vec<Position>>], start_position: Position) -> Vec<Vec<usize>> {
     let mut minimum_steps: Vec<Vec<usize>> = adjacency_list
         .iter()
         .map(|row| vec![usize::MAX; row.len()])
         .collect();
-    minimum_steps[position_start.0][position_start.1] = 0;
+    minimum_steps[start_position.0][start_position.1] = 0;
 
     let mut heap = BinaryHeap::new();
     heap.push(DijkstraState {
-        position: position_start,
+        position: start_position,
         steps: 0,
     });
 
     while let Some(DijkstraState { position, steps }) = heap.pop() {
-        if position.0 == position_end.0 && position.1 == position_end.1 {
-            // We reach the destination
-            break;
-        }
         if steps > minimum_steps[position.0][position.1] {
             // Skip since taking the current path takes a higher number of steps
             continue;
         }
-
         // For each node we can reach, see if we can find a way with
         // a lower cost going through this node
         for &neighbouring_position in &adjacency_list[position.0][position.1] {
@@ -71,22 +62,27 @@ fn dijkstra(
         }
     }
 
-    minimum_steps[position_end.0][position_end.1]
+    minimum_steps
 }
 
-fn part01(adjacency_list: &[Vec<Vec<Position>>], position_start: Position, position_end: Position) {
-    let minimum_step = dijkstra(adjacency_list, position_start, position_end);
-    println!("{}", minimum_step);
+fn part01(
+    adjacency_list: &[Vec<Vec<Position>>],
+    start_positioin: Position,
+    end_position: Position,
+) {
+    let minimum_steps = dijkstra(adjacency_list, start_positioin);
+    println!("{}", minimum_steps[end_position.0][end_position.1]);
 }
 
 fn part02(
     adjacency_list: &[Vec<Vec<Position>>],
-    potential_starts: &[Position],
-    position_end: Position,
+    start_position: Position,
+    end_positions: &[Position],
 ) {
+    let minimum_steps = dijkstra(adjacency_list, start_position);
     let mut minimum_step = usize::MAX;
-    for &position_start in potential_starts {
-        minimum_step = minimum_step.min(dijkstra(adjacency_list, position_start, position_end));
+    for pos in end_positions {
+        minimum_step = minimum_step.min(minimum_steps[pos.0][pos.1]);
     }
     println!("{}", minimum_step);
 }
@@ -99,8 +95,8 @@ fn main() {
     let file = File::open(&fpath).unwrap();
     let reader = BufReader::new(file);
 
-    let mut position_start = (0, 0);
-    let mut position_end = (0, 0);
+    let mut start_position = (0, 0);
+    let mut end_position = (0, 0);
     let mut grid = Vec::default();
 
     for (i, line) in reader.lines().enumerate() {
@@ -109,13 +105,13 @@ fn main() {
         for (j, c) in line.trim().chars().enumerate() {
             let c = match c {
                 'S' => {
-                    position_start.0 = i;
-                    position_start.1 = j;
+                    start_position.0 = i;
+                    start_position.1 = j;
                     'a'
                 }
                 'E' => {
-                    position_end.0 = i;
-                    position_end.1 = j;
+                    end_position.0 = i;
+                    end_position.1 = j;
                     'z'
                 }
                 c => c,
@@ -126,37 +122,44 @@ fn main() {
     }
 
     let mut potential_starts = Vec::default();
-    let mut adjacency_list: Vec<Vec<Vec<Position>>> = grid
+    let mut adjacency_list_p1: Vec<Vec<Vec<Position>>> = grid
+        .iter()
+        .map(|row| row.iter().map(|_| Vec::default()).collect())
+        .collect();
+    let mut adjacency_list_p2: Vec<Vec<Vec<Position>>> = grid
         .iter()
         .map(|row| row.iter().map(|_| Vec::default()).collect())
         .collect();
 
     for (i, row) in grid.iter().enumerate() {
         for (j, &c) in row.iter().enumerate() {
+            let current_position = (i, j);
             if c == 'a' {
-                potential_starts.push((i, j));
+                potential_starts.push(current_position);
             }
-            // Find neighbours that can be visited
-            adjacency_list[i][j].extend(
-                [
-                    (i.saturating_sub(1), j),
-                    (i.saturating_add(1).min(grid.len() - 1), j),
-                    (i, j.saturating_sub(1)),
-                    (i, j.saturating_add(1).min(row.len() - 1)),
-                ]
-                .into_iter()
-                .filter(|&pos| {
-                    if i == pos.0 && j == pos.1 {
-                        return false;
-                    }
-                    let h1 = c as u32;
-                    let h2 = grid[pos.0][pos.1] as u32;
-                    h2 <= h1 || h2 - h1 == 1
-                }),
-            );
+
+            let potential_neighbours = [
+                (i.saturating_sub(1), j),
+                (i.saturating_add(1).min(grid.len() - 1), j),
+                (i, j.saturating_sub(1)),
+                (i, j.saturating_add(1).min(row.len() - 1)),
+            ];
+            for pos in potential_neighbours {
+                if pos == current_position {
+                    continue;
+                }
+                let h1 = c as u32;
+                let h2 = grid[pos.0][pos.1] as u32;
+                if h2 <= h1 || h2 - h1 == 1 {
+                    adjacency_list_p1[i][j].push(pos);
+                }
+                if h2 >= h1 || h1 - h2 == 1 {
+                    adjacency_list_p2[i][j].push(pos);
+                }
+            }
         }
     }
 
-    part01(&adjacency_list, position_start, position_end);
-    part02(&adjacency_list, &potential_starts, position_end);
+    part01(&adjacency_list_p1, start_position, end_position);
+    part02(&adjacency_list_p2, end_position, &potential_starts);
 }
